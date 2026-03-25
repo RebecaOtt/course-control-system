73d5d8cc26fb077629fc0479a4449a0cfff4817c
RebecaOtt <rebecaaott@gmail.com>
2026-03-25 09:47:38 -0300

    fazendo métodos de cadastrar, listar e buscar aluno

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,94 @@
+import model.Student;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("postgres");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
 
-        entityManager.close();
-        entityManagerFactory.close();
+        Student student = new Student();
+        System.out.println("0- Cadastrar aluno, 1- Listar aluno, 2- Buscar aluno");
+        int studentOption = scanner.nextInt();
+
+        if (studentOption < 0 || studentOption > 2){
+            System.out.println("Opção inválida");
+        } else {
+            switch (studentOption) {
+                case 0:
+                    System.out.println("Digite seu nome:");
+                    scanner.nextLine();
+                    String studentName = scanner.nextLine();
+                    student.setName(studentName);
+
+                    System.out.println("Digite seu email:");
+                    String studentEmail = scanner.next();
+                    student.setEmail(studentEmail);
+
+                    System.out.println("Digite sua data de nascimento:");
+                    String date = scanner.next();
+                    try {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                        LocalDate dateOfBirth = LocalDate.parse(date, formatter);
+                        student.setDateOfBirth(dateOfBirth);
+                    } catch (Exception e) {
+                        System.out.println("Data inválida, use o formato dd/MM/yyyy");
+                        break;
+                    }
+
+                    entityManager.getTransaction().begin(); //inicia transição
+                    entityManager.persist(student); //marca o objeto para salvar
+                    entityManager.getTransaction().commit(); //insere o aluno no bd
+
+                    System.out.println("Aluno cadastrado com sucesso!");
+                    break;
+                case 1:
+
+                    entityManager.getTransaction().begin();
+                    List<Student> studentsList = entityManager.createQuery("SELECT students FROM Student students", Student.class).getResultList();
+
+                    for (Student students : studentsList) {
+                        System.out.println("ID: " + students.getId());
+                        System.out.println("Nome: " + students.getName());
+                        System.out.println("Email: " + students.getEmail());
+                        System.out.println("Data de nascimento: " + students.getDateOfBirth());
+                        System.out.println("------------------------");
+                    }
+                    entityManager.getTransaction().commit();
+                    break;
+                default:
+                    System.out.println("Digite seu email:");
+                    String searchEmail = scanner.next();
+
+                    entityManager.getTransaction().begin();
+
+                    List<Student> students = entityManager
+                            .createQuery("SELECT searchStudents FROM Student searchStudents WHERE searchStudents.email = :email", Student.class)
+                            .setParameter("email", searchEmail)
+                            .getResultList();
+
+                    if (students.isEmpty()) {
+                        System.out.println("Aluno não encontrado.");
+                    } else {
+                        for (Student searchStudents : students) {
+                            System.out.println("Nome: " + searchStudents.getName());
+                            System.out.println("Email: " + searchStudents.getEmail());
+                            System.out.println("Data: " + searchStudents.getDateOfBirth());
+                        }
+                    }
+                    entityManager.getTransaction().commit();
+            }
+
+            entityManager.close();
+            entityManagerFactory.close();
+        }
     }
 }
--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -19,4 +19,37 @@ public class Student {
     //um aluno pode ter se matriculado em varios cursos
     @OneToMany(mappedBy = "student")
     private List<Registration> registrationList;
+
+    //Getters e Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
